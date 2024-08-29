ec2aab095424572d78f464a0c38a9b88b36cbfda
droy <royd4405aoi@gmail.com>
2024-08-28 21:37:48 -0700

    database-created

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from fastapi import FastAPI, status
+from database import Base, engine
+from pydantic import BaseModel
+
+class ToDoRequest(BaseModel):
+    task: str
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return "todooo"
 
+
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo():
+def create_todo(todo: ToDoRequest):
     return "create todo item"
+
 
 @app.get("/todo/{id}")
 def read_todo(id: int):
     return "read todo item with id {id}"
 
+
 @app.put("/todo/{id}")
 def update_todo(id: int):
     return "update todo item with id {id}"
 
+
 @app.delete("/todo/{id}")
 def delete_todo(id: int):
     return "delete todo item with id {id}"
+
 
 @app.get("/todo")
 def read_todo_list():
